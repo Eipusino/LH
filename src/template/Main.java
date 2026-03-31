2e76c5f8d1e89b1825560741722c94b9959e7110
Eipusino <wdnmddd1@outlook.com>
2026-03-31 19:38:58 +0800

    update 26.3.31

--- src/template/Main.java
+++ src/template/Main.java
@@ -2,6 +2,7 @@ package template;
 
 import sun.misc.Unsafe;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
@@ -10,34 +11,16 @@ import java.util.Arrays;
 
 @SuppressWarnings("removal")
 public class Main {
-	static final Unsafe unsafe;
-	static final Lookup lookup;
-
-	final Object obj = null;
+	//static final Unsafe unsafe;
+	//static final Lookup lookup;
 
 	static {
-		try {
-			Field field = Unsafe.class.getDeclaredField("theUnsafe");
-			field.setAccessible(true);
-			unsafe = (Unsafe) field.get(null);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
 
-		try {
-			Field field = Lookup.class.getDeclaredField("IMPL_LOOKUP");
-			field.setAccessible(true);
-			lookup = (Lookup) field.get(null);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	static void main(String... args) {
 		try {
-			Field field = Main.class.getDeclaredField("obj");
-			field.setAccessible(true);
-			field.set(new Main(), "new Object()\\{}");
+			System.out.println("oh no");
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
